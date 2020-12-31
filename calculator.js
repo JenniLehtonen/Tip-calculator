c3de5955a1c93907d0cf8d277885d24928f0dc99
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2020-12-31 17:33:31 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,9 +8,14 @@ var tipProcent = 15;
     document.getElementById("tip").innerHTML=tipProcent + " %";
   }
   function calculateTip(bill, people){  //calculate the amount of the tip and show it
-    var calculatedTip = bill/100*tipProcent;
-    var tip = calculatedTip/people;
-    var fixedTip = tip.toFixed(2);
-    document.getElementById('totalTip').innerHTML = fixedTip + " €";
+   if (bill=== "" || people ===  ""){
+      document.getElementById("totalTip").innerHTML = "0.00 €";
+    } else {
+      var calculatedTip = bill/100*tipProcent;
+      var tip = calculatedTip/people;
+      var fixedTip = tip.toFixed(2);
+      document.getElementById("totalTip").innerHTML = fixedTip + " €";
+    }
+    
   }
     
